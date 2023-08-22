de631ca4cfc6ee296becfd20f52b5854da85b843
Frank <andreass.frankk@gmail.com>
2023-08-22 15:33:56 +0300

    fixed cros

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
+import cors from 'cors'
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
@@ -19,6 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors())
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
