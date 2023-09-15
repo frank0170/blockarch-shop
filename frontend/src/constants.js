eee4883013ccb1a31b537fe7e0baa8664defc88c
Frank <andreass.frankk@gmail.com>
2023-09-15 11:19:24 +0300

    added

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5050' : '';
 export const BASE_URL = ''; // If using proxy
-export const PRODUCTS_URL = '/api/products';
+export const PRODUCTS_URL = '/.netlify/functions/get_server';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
 export const PAYPAL_URL = '/api/config/paypal';
